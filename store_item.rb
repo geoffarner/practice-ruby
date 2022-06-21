3517225f5cc256e1b4c31444975c56ef5f856eb7
Geoffrey Arner <geoffarner5@gmail.com>
2022-06-21 14:30:21 -0500

    store items hash

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+dog_1 = { breed: "terrier", color: "black", size: "small", price: 100 }
+dog_2 = { breed: "golden retriever", color: "yellow", size: "Medium", price: 300 }
+dog_3 = { :breedv => "pointer", :color => "white and brown", :size => "Medium", price: 500 }
+
+puts "A #{dog_1[:breed]} is #{dog_1[:color]} and #{dog_1[:size]} and costs #{dog_1[:price]}."
